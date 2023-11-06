dcb2fc70981c8e6f428e1a2a04e873405b8dda1f
black-adm <mattheuscontato17@gmail.com>
2023-11-06 19:16:04 -0300

    create transaction simple test

--- test/example.spec.ts
+++
@@ -1,9 +0,0 @@
-import { expect, test } from 'vitest'
-import { app } from '../src/app'
-
-import request from 'supertest'
-
-test('O usuário deve ser capaz de criar uma nova transação', () => {
-  const responseStatusCode = 201
-  expect(responseStatusCode).toEqual(201)
-})
