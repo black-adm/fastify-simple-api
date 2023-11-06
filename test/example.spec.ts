5f7e0cb661a1d1964d17dce919f6aca8474e6862
black-adm <mattheuscontato17@gmail.com>
2023-11-06 19:07:55 -0300

    Installing supertest and separating logic from running the server

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,7 +1,9 @@
 import { expect, test } from 'vitest'
+import { app } from '../src/app'
+
+import request from 'supertest'
 
 test('O usuário deve ser capaz de criar uma nova transação', () => {
   const responseStatusCode = 201
-
   expect(responseStatusCode).toEqual(201)
 })
